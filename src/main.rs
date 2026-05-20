8322e4b05d9e0a9daf20bce65edd5bf300a75199
EbaraKoji <kojieva5270@gmail.com>
2026-05-20 16:20:33 +0900

    add more routing

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,20 @@ use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let app = Router::new().route("/", get(|| async { "hello, world!" }));
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/about", get(about));
 
     let endpoint = "0.0.0.0:8000";
     let listener = tokio::net::TcpListener::bind(endpoint).await?;
     println!("Listening on {endpoint}...");
 
     axum::serve(listener, app).await
+}
+
+async fn index() -> &'static str {
+    "Home"
+}
+async fn about() -> &'static str {
+    "About"
 }
