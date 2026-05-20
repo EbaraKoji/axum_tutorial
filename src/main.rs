91f3cca58203351d05d26e217dd944a714f0524a
EbaraKoji <kojieva5270@gmail.com>
2026-05-20 16:47:58 +0900

    return different response types

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::io;
 
-use axum::{Router, extract::Path, routing::get};
+use axum::{
+    Json, Router,
+    http::StatusCode,
+    response::Html,
+    routing::{get, post},
+};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let user_routes = Router::new()
-        .route("/", get(list_users).post(create_user))
-        .route("/{id}", get(get_user).put(update_user).delete(delete_user));
-    let health_routes = Router::new().route("/api/health", get(health));
-
     let app = Router::new()
-        .nest("/api/users", user_routes)
-        .merge(health_routes);
-
+        .route("/plain", get(plain))
+        .route("/no-content", get(no_content))
+        .route("/json", get(json))
+        .route("/page", get(page))
+        .route("/create", post(created));
     let endpoint = "0.0.0.0:8000";
     let listener = tokio::net::TcpListener::bind(endpoint).await?;
     println!("Listening on {endpoint}...");
@@ -20,26 +22,27 @@ async fn main() -> io::Result<()> {
     axum::serve(listener, app).await
 }
 
-async fn list_users() -> &'static str {
-    "List users"
+/// Returns plain text.
+async fn plain() -> &'static str {
+    "Hello"
 }
 
-async fn create_user() -> &'static str {
-    "Create user"
+/// Returns Status code only.
+async fn no_content() -> StatusCode {
+    StatusCode::NO_CONTENT
 }
 
-async fn get_user(Path(id): Path<String>) -> String {
-    format!("User id: {id}")
+/// Returns JSON.
+async fn json() -> Json<serde_json::Value> {
+    Json(serde_json::json!({"message": "Hello"}))
 }
 
-async fn update_user(Path(id): Path<String>) -> String {
-    format!("Updating user(id: {id})")
+/// Returns HTML.
+async fn page() -> Html<&'static str> {
+    Html("<h1>Hello</h1>")
 }
 
-async fn delete_user(Path(id): Path<String>) -> String {
-    format!("Delete user(id: {id})")
-}
-
-async fn health() -> &'static str {
-    "Axum api is working!"
+/// Returns Tuple: (StatusCode, Body)
+async fn created() -> (StatusCode, Json<serde_json::Value>) {
+    (StatusCode::CREATED, Json(serde_json::json!({"id": 1})))
 }
