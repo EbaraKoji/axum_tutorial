62c8294199fa64cf8ec1b575229fe1e7922e3203
EbaraKoji <kojieva5270@gmail.com>
2026-05-20 18:00:16 +0900

    create basic state example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use std::io;
+use std::{
+    io,
+    sync::{Arc, Mutex},
+};
 
-use axum::{Router, extract::FromRequestParts, http::StatusCode, routing::get};
+use axum::{Router, extract::State, routing::post};
+
+#[derive(Clone)]
+struct AppState {
+    count: Arc<Mutex<i32>>,
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let app = Router::new().route("/user", get(profile));
+    let app_state = AppState {
+        count: Arc::new(Mutex::new(1)),
+    };
+    let app = Router::new()
+        .route("/increment", post(increment))
+        .route("/decrement", post(decrement))
+        .with_state(app_state);
     let endpoint = "0.0.0.0:8000";
     let listener = tokio::net::TcpListener::bind(endpoint).await?;
     println!("Listening on {endpoint}...");
@@ -12,33 +26,12 @@ async fn main() -> io::Result<()> {
     axum::serve(listener, app).await
 }
 
-struct CurrentUser {
-    user_id: u64,
+async fn increment(State(state): State<AppState>) -> String {
+    *state.count.lock().unwrap() += 1;
+    format!("updated count to {}", state.count.lock().unwrap())
 }
 
-impl<S> FromRequestParts<S> for CurrentUser
-where
-    S: Send + Sync,
-{
-    type Rejection = StatusCode;
-
-    async fn from_request_parts(
-        parts: &mut axum::http::request::Parts,
-        _state: &S,
-    ) -> Result<Self, Self::Rejection> {
-        let user_id = parts
-            .headers
-            .get("x-user-id")
-            .and_then(|v| v.to_str().ok())
-            .and_then(|v| v.parse().ok())
-            .ok_or(StatusCode::UNAUTHORIZED)?;
-
-        Ok(CurrentUser { user_id })
-    }
-}
-
-/// Get Current User.
-/// example: `curl http://localhost:8000/user -H "x-user-id: 5"`
-async fn profile(user: CurrentUser) -> String {
-    format!("User #{}", user.user_id)
+async fn decrement(State(state): State<AppState>) -> String {
+    *state.count.lock().unwrap() -= 1;
+    format!("updated count to {}", state.count.lock().unwrap())
 }
