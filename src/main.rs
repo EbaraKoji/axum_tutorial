cc9bac92dc594a8235533ec7932d5401d0d4d612
EbaraKoji <kojieva5270@gmail.com>
2026-05-20 16:40:56 +0900

    add merging routers

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,11 @@ async fn main() -> io::Result<()> {
     let user_routes = Router::new()
         .route("/", get(list_users).post(create_user))
         .route("/{id}", get(get_user).put(update_user).delete(delete_user));
+    let health_routes = Router::new().route("/api/health", get(health));
 
-    let app = Router::new().nest("/api/users", user_routes);
+    let app = Router::new()
+        .nest("/api/users", user_routes)
+        .merge(health_routes);
 
     let endpoint = "0.0.0.0:8000";
     let listener = tokio::net::TcpListener::bind(endpoint).await?;
@@ -35,4 +38,8 @@ async fn update_user(Path(id): Path<String>) -> String {
 
 async fn delete_user(Path(id): Path<String>) -> String {
     format!("Delete user(id: {id})")
+}
+
+async fn health() -> &'static str {
+    "Axum api is working!"
 }
